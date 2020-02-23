43cb03f3b06625bc4a27f1fa9ac2ded27d37dec6
someone <someone>
2020-02-22 21:09:58 -0600

    Horizontal working with some index errors

--- src/main/java/wordsearchkata/CrossWord.java
+++ src/main/java/wordsearchkata/CrossWord.java
@@ -4,7 +4,6 @@
 package wordsearchkata;
 
 import java.io.*;
-import java.util.Arrays;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.BufferedReader;
@@ -18,11 +17,9 @@ public class CrossWord {
         String[][] allWords = null; //Crossword in Array
         String[] lineArray = null;
         String nextLine = null;
-        int[] firstLoc = new int[2];
         int numLines = 0;
         int locX = 0;
         int locY = 0;
-
 
         // This is the pulling of the text file and population of the wordList and actual crossword.
         String filePath = "/Users/labateje/wordsearchkata/src/main/java/wordsearchkata/WordList";
@@ -38,7 +35,6 @@ public class CrossWord {
 
             allWords = new String[numLines][numLines];
 
-
             for(int i=1;i<=numLines;i++) {
                 nextLine = Files.readAllLines(Paths.get("/Users/labateje/tddwordsearch/src/main/java/tddwordsearch/WordList")).get(i);
                 lineArray = nextLine.split(",");
@@ -47,38 +43,54 @@ public class CrossWord {
                 }
             }
 
-
-
-
             lineReader.close();
         } catch (IOException ex) {
             System.err.println(ex);
         }
+        /* To test print crossword
+        for (int l = 0; l < numLines; l++) {
+            for (int r = 0; r < numLines; r++) {
 
-        // This is the start of the actual search.
-        String[] outPut = new String[inputWord.length()*2];
-        String[] otherLocs = new String[(inputWord.length()-1)*2];
-        String firstCoords = "";
-        firstLoc = firstLetter(allWords, numLines, inputWord);
-        locX = firstLoc[0];
-        locY = firstLoc[1];
-        //System.out.printf("%s\n", inputWord.substring(0, 1));
-        //System.out.printf("(%s,%s)\n", locX, locY);
-        outPut[0] = "("+locX+","+locY+")";
-        firstCoords = "("+locX+","+locY+")";
+                System.out.print(allWords[l][r] + " ");
+                if(r==14){
+                    System.out.println();
+                }
+            }
+        }
+        */
+        String outPutFinal = "";
+        int r = 0;
+        outerloop:
+        while(true){
+            // This is the start of the actual search.
+            String firstCoords = "";
 
-        otherLocs = searchHorizontally(allWords, inputWord, locX, locY);
+            //firstLoc = firstLetter(allWords, numLines, inputWord); working
+            //locX = firstLoc[0];
+            //locY = firstLoc[1];
+            int[][] firstLoc = firstLetter(allWords, numLines, inputWord);
+            locX = firstLoc[r][0];
+            locY = firstLoc[r][1];
+            //System.out.printf("%s, %s\n", locX, locY);
+            //System.out.printf("%s\n", inputWord.substring(0, 1));
+            //System.out.printf("(%s,%s)\n", locX, locY);
 
-
-        String outPutString = String.join(",", otherLocs);
-        outPutString = inputWord+": "+firstCoords+","+outPutString;
-        System.out.printf("%s\n", outPutString);
-
-        return outPutString;
+            firstCoords = "("+locX+","+locY+")";
+            String[] otherLocs = searchHorizontally(allWords, inputWord, locX, locY);
+            String outPutString = String.join(",", otherLocs);
+            outPutString = inputWord+": "+firstCoords+","+outPutString;
+            System.out.printf("%s\n", outPutString);
+            outPutFinal = outPutString;
+            if(otherLocs[0] != null){
+                break outerloop;
+            }
+            r++;
+        }
+        return outPutFinal;
     }
 
-
-    public int[] firstLetter(String[][] allWords, int numLines, String inputWord){
+    /*
+    public int[] firstLetter(String[][] allWords, int numLines, String inputWord){ working
         int[] firstLoc = new int[2];
         outerloop:
         for (int l = 0; l < numLines; l++) {
@@ -93,20 +105,36 @@ public class CrossWord {
         }
         return firstLoc;
     }
+    */
+    public int[][] firstLetter(String[][] allWords, int numLines, String inputWord){
+        int[][] firstLoc = new int[numLines][numLines];
+        int p =0;
+        for (int l = 0; l < numLines; l++) {
+            for (int r = 0; r < numLines; r++) {
+                if (allWords[l][r].equals(inputWord.substring(0, 1))) {
+                    //System.out.printf("(%s,%s)\n", l, r);
+                    firstLoc[p][0] = l;
+                    firstLoc[p][1] = r;
+                    p++;
+                }
+            }
+        }
+        return firstLoc;
+    }
 
     public String[] searchHorizontally(String[][] allWords, String inputWord, int firstX, int firstY){
         String[] outPut = new String[inputWord.length()-1];
         //Forwards
         for(int i = 1; i < inputWord.length();i++){
             if(allWords[firstX][firstY+i].equals(inputWord.substring(i, i+1))){
-                System.out.printf("(%s,%s)\n", firstX, firstY+i);
+                //System.out.printf("(%s,%s)\n", firstX, firstY+i);
                 outPut[i-1] = "("+firstX+","+(firstY+i)+")";
             }
         }
         //Backwards
         for(int i = 1; i < inputWord.length();i++){
             if(allWords[firstX][firstY-i].equals(inputWord.substring(i, i+1))){
-                System.out.printf("(%s,%s)\n", firstX, firstY-i);
+                //System.out.printf("(%s,%s)\n", firstX, firstY-i);
                 outPut[i-1] = "("+firstX+","+(firstY-i)+")";
             }
         }
--- src/test/java/wordsearchkata/CrossWordTest.java
+++ src/test/java/wordsearchkata/CrossWordTest.java
@@ -36,4 +36,45 @@ public class CrossWordTest {
 
         assertEquals("JVN: (0,10),(0,9),(0,8)", crossWord.word("JVN"));
     }
+
+    @Test public void shouldReturnKIN() {
+
+        assertEquals("KIN: (0,6),(0,7),(0,8)", crossWord.word("KIN"));
+    }
+
+    @Test public void shouldReturnBRJS() {
+
+        assertEquals("BRJS: (3,0),(3,1),(3,2),(3,3)", crossWord.word("BRJS"));
+    }
+
+    @Test public void shouldReturnZZ() {
+
+        assertEquals("ZZ: (1,5),(1,6)", crossWord.word("ZZ"));
+    }
+
+    @Test public void shouldReturnXT() {
+
+        assertEquals("XT: (2,12),(2,13)", crossWord.word("XT"));
+    }
+
+    @Test public void shouldReturnETIK() {
+
+        assertEquals("ETIK: (3,7),(3,8),(3,9),(3,10)", crossWord.word("ETIK"));
+    }
+
+    @Test public void shouldReturnLLSH() {
+
+        assertEquals("LLSH: (1,0),(1,1),(1,2),(1,3)", crossWord.word("LLSH"));
+    }
+
+    @Test public void shouldReturnCOTTYK() {
+
+        assertEquals("COTTYK: (5,1),(5,2),(5,3),(5,4),(5,5),(5,6)", crossWord.word("COTTYK"));
+    }
+
+    @Test public void shouldReturnSCOTTY() {
+
+        assertEquals("SCOTTY: (0,5),(1,5),(2,5),(3,5),(4,5),(5,5)", crossWord.word("SCOTTY"));
+    }
 }
+
